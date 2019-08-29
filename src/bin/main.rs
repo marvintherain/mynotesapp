b3a3db5c1d6d800f27049dc3a0b4b4817bd04507
marvintherain <marvintherain@github.com>
2019-08-29 09:19:34 +0200

    added cli selector and edit dialogue

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,7 +5,7 @@ use self::mynotesapp::*;
 use self::models::*;
 use self::diesel::prelude::*;
 
-use dialoguer::{theme::ColorfulTheme, Select};
+use dialoguer::{theme::ColorfulTheme, Select, Editor};
 
 fn main() {
     let connection = establish_connection();
@@ -20,44 +20,41 @@ fn main() {
 //-------------------------------------------------
 
     use schema::notes;
-
-    // let my_notes: Vec<(i32, String, chrono::NaiveDateTime, chrono::NaiveDateTime)> = notes::table
-    //     .select((notes::id,
-    //              notes::title, 
-    //              notes::creation_date, 
-    //              notes::last_edit))
-    //     .load(&connection)
-    //     .expect("Error loading notes");
     
     let my_notes = notes::table
      .load::<Note>(&connection)
      .expect("Error loading notes");
 
-    println!("{:?}\n", my_notes[0].title);
     let mut ui_selections: Vec<String> = vec![];
-    // println!("title    creation date    last edit\n");
     let my_copy = my_notes.clone();
     for entry in my_notes {
         let entry_creation = entry.creation_date.format("%Y-%m-%d %H:%M:%S").to_string();
         let new_selection = String::from(entry.title + " " + &entry_creation);
         ui_selections.push(new_selection);
     }
-    // println!("{:?}", ui_selections);
 
 //-------------------------------------------------
     
-
-    // let ui_selections = &my_notes;
-
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("select the note you want to edit\ntitle              creation date")
         .default(0)
         .items(&ui_selections[..])
         .interact()
         .unwrap();
-    println!("Enjoy your {}!", ui_selections[selection]);
-    println!("{:?}", my_copy[selection]);
+    // println!("Enjoy your {}!", ui_selections[selection]);
+    // println!("{:?}", my_copy[selection]);
 
+    let original_post = &my_copy[selection].body.as_ref().unwrap();
+
+    if let Some(rv) = Editor::new().edit(original_post).unwrap() {
+        let modded_note = mod_note(&connection, my_copy[selection].id, None, Some(&rv));
+        println!("Note {} changed!", modded_note.title);
+    } else {
+        println!("Abort");
+    }
+
+  //-------------------------------------------------
+  
 
     // let deleted = delete_note(&connection, 2);
 
